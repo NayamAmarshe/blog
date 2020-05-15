84cd857c4697dadfcbb2893ed05af66928faac3e
Dathomir <mayankvallah@gmail.com>
2020-05-16 03:06:40 +0530

    updated app.js

--- app.js
+++ app.js
@@ -12,21 +12,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
