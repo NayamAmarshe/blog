b71e96ee58c1ccb11a16a7afb430b472f77f6958
Dathomir <mayankvallah@gmail.com>
2020-06-12 23:46:27 +0530

    Added lodash

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
+
+//consts
+var posts = [];
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -16,7 +20,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-var posts = [];
 
 //routes
 app.get("/", (req, res) => {
@@ -30,21 +33,21 @@ app.get("/about", (req, res) => {
   res.render("about", {
     aboutContent: aboutContent
   });
-})
+});
 
 app.get("/contact", (req, res) => {
   res.render("contact", {
     contactContent: contactContent
   });
-})
+});
 
 app.get("/compose", (req, res) => {
   res.render("compose", {});
-})
+});
 
 app.post("/", (req, res) => {
   res.send('POST request to the homepage')
-})
+});
 
 app.post("/compose", (req, res) => {
   const post = {
@@ -53,7 +56,20 @@ app.post("/compose", (req, res) => {
   }
   posts.push(post);
   res.redirect("/");
-})
+});
+
+app.get("/posts/:post", (req, res) => {
+  posts.forEach(function(post) {
+    if (_.lowerCase(req.params.post) === _.lowerCase(post.title)) {
+      res.render("post", {
+        pageTitle: post.title,
+        pageBody: post.body
+      });
+    } else {
+      console.log("No Match Found!");
+    }
+  });
+});
 
 //listening
 app.listen(3000, function() {
